ead9a090a8ed38f490025ae542335b887fcac81e
Farhan Ali <87860980+farhanaliofficial@users.noreply.github.com>
2022-07-31 10:00:01 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Govt Graduate College Vehari</title>
+	<link rel="stylesheet" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<nav class="nav">
+		<div class="brand">
+			<a href="#">Govt Graduate College</a>
+		</div>
+		<div class="bars">&#9776;</div>
+	</nav>
+	<div class="wrapper">
+      <ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">Gallery</a></li>
+<li><a href="#">Feedback</a></li>
+</ul>
+</div>
+	<div class="container">
+		<div class="form">
+			<div class="header">
+				<h2>Login Now</h2>
+			</div>
+			<form action="" method="POST">
+				<input type="email" name="email" class="form-control" placeholder="Enter Your Email" required autocomplete="off">
+				<input type="password" name="password" class="form-control" placeholder="Enter Your Password" required autocomplete="off">
+				<input type="submit" value="Login" name="login" class="btn btn-primary">
+				<span>Not Have an Account! <a href="#">Sign Up Now!</a></span>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
